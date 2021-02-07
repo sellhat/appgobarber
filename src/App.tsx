24f1041841181f4d20e052bbfe6610eb59315c6b
sellhat <programadoram@hotmail.com>
2021-02-07 18:53:43 -0300

    first commit - mobile gobarber

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import 'react-native-gesture-handler';
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {StatusBar, View} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Routes from './routes';
+
+const App: React.FC = () => (
+  <NavigationContainer>
+    <StatusBar barStyle="light-content" backgroundColor="#312E38" />
+    <View style={{flex: 1, backgroundColor: '#312E38'}}>
+      <Routes />
+    </View>
+  </NavigationContainer>
+);
+
+export default App;
